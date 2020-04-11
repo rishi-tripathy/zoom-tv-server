1e09835f2295db092c559c3ef7892a5503fa7b92
jwei98 <justin.lj.wei@gmail.com>
2020-04-11 14:59:02 -0700

    add route for retrieving events

---
+++ auth.py
@@ -0,0 +1,34 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def get_calendar_service():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'secrets/webserver_gcal_api_client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+    return service
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+
+import auth
+import datetime
+import quickstart
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +12,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, world! Testing CloudBuild'
+
+
+@app.route('/events')
+def events():
+    service = auth.get_calendar_service()
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                          maxResults=10, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    """
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+    """
+    return events[0]['summary']
